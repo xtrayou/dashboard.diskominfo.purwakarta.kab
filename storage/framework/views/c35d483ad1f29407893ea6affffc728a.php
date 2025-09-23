6ace6eb91d1188dfd19443544384ed5e937bd813
silmi yushini <2210631250098@student.unsika.ac.id>
2025-09-23 15:38:26 +0700

    Initial commit

---
+++ storage/framework/views/c35d483ad1f29407893ea6affffc728a.php
@@ -0,0 +1,8 @@
+<section
+    <?php echo e($attributes->merge(['class' => "w-full max-w-7xl mx-auto p-4 sm:p-14 border-x border-dashed border-neutral-300 dark:border-white/[9%]"])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</section>
+<?php /**PATH D:\SILMI\magang\dashboard\dashboard.diskominfo.purwakarta.kab\vendor\laravel\framework\src\Illuminate\Foundation\resources\exceptions\renderer\components\section-container.blade.php ENDPATH**/ ?>
